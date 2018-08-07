7056c0b788fe47e4528292cd8486b9c3049f8c16
foongming <laifoongming@gmail.com>
2018-08-07 10:05:48 +0800

    Lesson13Quiz3.py solved

--- Lesson13Quiz3.py
+++ Lesson13Quiz3.py
@@ -8,7 +8,7 @@
 
 def list_mean(x):
     n = len(x)
-    hold = 0
+    hold = 0.0
     while len(x) > 0:
         hold = hold + x.pop()
     average = hold / n
@@ -19,9 +19,11 @@ print list_mean([1,2,3,4])
 #>>> 2.5
 print list_mean([1,3,4,5,2])
 #>>> 3.0
+
 #print list_mean([])
 #>>> ??? You decide. If you decide it should give an error, comment
 # out the print line above to prevent your code from being graded as
 # incorrect.
+
 print list_mean([2])
 #>>> 2.0
